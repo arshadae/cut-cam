DDP gradient synchronization bug removal during training

models/networks.py
@@ -19,6 +19,11 @@ Modification Log:
 - All conditional statements that check for opt.gpu_ids have been modified accordingly.
 - Added device handling to ensure that the MLP is created on the same device as the features, with mlp = mlp.to(feat.device)  in create_mlp() method.
 - Adusted PatchSampleF.forward() to avoid the warning for calling torch.tensor on an existing tensor, by using .clone().detach() when patch_id is already a tensor.
+- Made Downsample conv weights per-call, non-mutating, and dtype/device-safe in Downsample.forward() method.
+- Made Upsample conv weights per-call, non-mutating, and dtype/device-safe in Upsample.forward() method.
+- Disabled in-place activations to inplace=False
+- Remove the in-place residual add in ResBlock.forward
+- Updated Downsample.forward() and Upsample.forward() to make the per-call weight an independent tensor that cannot share storage with any mutable buffer/hook:
 """
 
 def get_filter(filt_size=3):
@@ -60,13 +65,14 @@ class Downsample(nn.Module):
         self.pad = get_pad_layer(pad_type)(self.pad_sizes)
 
     def forward(self, inp):
-        if(self.filt_size == 1):
-            if(self.pad_off == 0):
-                return inp[:, :, ::self.stride, ::self.stride]
-            else:
-                return self.pad(inp)[:, :, ::self.stride, ::self.stride]
-        else:
-            return F.conv2d(self.pad(inp), self.filt, stride=self.stride, groups=inp.shape[1])
+        if self.filt_size == 1:
+            return (inp if self.pad_off == 0 else self.pad(inp))[:, :, ::self.stride, ::self.stride]
+        # make a fresh, detached copy on the right device/dtype
+        weight = (self.filt.to(dtype=inp.dtype, device=inp.device)
+                        .detach()     # break any aliasing
+                        .clone()      # own storage for safety
+                        .contiguous())
+        return F.conv2d(self.pad(inp), weight, stride=self.stride, groups=self.channels)
 
 
 class Upsample2(nn.Module):
@@ -94,12 +100,16 @@ class Upsample(nn.Module):
 
         self.pad = get_pad_layer(pad_type)([1, 1, 1, 1])
 
+    # Upsample.forward
     def forward(self, inp):
-        ret_val = F.conv_transpose2d(self.pad(inp), self.filt, stride=self.stride, padding=1 + self.pad_size, groups=inp.shape[1])[:, :, 1:, 1:]
-        if(self.filt_odd):
-            return ret_val
-        else:
-            return ret_val[:, :, :-1, :-1]
+        weight = (self.filt.to(dtype=inp.dtype, device=inp.device)
+                        .detach()
+                        .clone()
+                        .contiguous())
+        ret = F.conv_transpose2d(self.pad(inp), weight, stride=self.stride,
+                                padding=1 + self.pad_size, groups=self.channels)[:, :, 1:, 1:]
+        return ret if self.filt_odd else ret[:, :, :-1, :-1]
+
 
 
 def get_pad_layer(pad_type):
@@ -789,7 +799,7 @@ class ResBlock(nn.Module):
     def forward(self, x):
         residual = x
         out = self.model(x)
-        out += residual
+        out = out + residual
         return out
 
 
@@ -821,13 +831,13 @@ class Conv2dBlock(nn.Module):
 
         # initialize activation
         if activation == 'relu':
-            self.activation = nn.ReLU(inplace=True)
+            self.activation = nn.ReLU(inplace=False)
         elif activation == 'lrelu':
-            self.activation = nn.LeakyReLU(0.2, inplace=True)
+            self.activation = nn.LeakyReLU(0.2, inplace=False)
         elif activation == 'prelu':
             self.activation = nn.PReLU()
         elif activation == 'selu':
-            self.activation = nn.SELU(inplace=True)
+            self.activation = nn.SELU(inplace=False)
         elif activation == 'tanh':
             self.activation = nn.Tanh()
         elif activation == 'none':
@@ -869,13 +879,13 @@ class LinearBlock(nn.Module):
 
         # initialize activation
         if activation == 'relu':
-            self.activation = nn.ReLU(inplace=True)
+            self.activation = nn.ReLU(inplace=False)
         elif activation == 'lrelu':
-            self.activation = nn.LeakyReLU(0.2, inplace=True)
+            self.activation = nn.LeakyReLU(0.2, inplace=False)
         elif activation == 'prelu':
             self.activation = nn.PReLU()
         elif activation == 'selu':
-            self.activation = nn.SELU(inplace=True)
+            self.activation = nn.SELU(inplace=False)
         elif activation == 'tanh':
             self.activation = nn.Tanh()
         elif activation == 'none':
@@ -948,7 +958,7 @@ class ResnetGenerator(nn.Module):
         model = [nn.ReflectionPad2d(3),
                  nn.Conv2d(input_nc, ngf, kernel_size=7, padding=0, bias=use_bias),
                  norm_layer(ngf),
-                 nn.ReLU(True)]
+                 nn.ReLU(False)]
 
         n_downsampling = 2
         for i in range(n_downsampling):  # add downsampling layers
@@ -956,11 +966,11 @@ class ResnetGenerator(nn.Module):
             if(no_antialias):
                 model += [nn.Conv2d(ngf * mult, ngf * mult * 2, kernel_size=3, stride=2, padding=1, bias=use_bias),
                           norm_layer(ngf * mult * 2),
-                          nn.ReLU(True)]
+                          nn.ReLU(False)]
             else:
                 model += [nn.Conv2d(ngf * mult, ngf * mult * 2, kernel_size=3, stride=1, padding=1, bias=use_bias),
                           norm_layer(ngf * mult * 2),
-                          nn.ReLU(True),
+                          nn.ReLU(False),
                           Downsample(ngf * mult * 2)]
 
         mult = 2 ** n_downsampling
@@ -976,7 +986,7 @@ class ResnetGenerator(nn.Module):
                                              padding=1, output_padding=1,
                                              bias=use_bias),
                           norm_layer(int(ngf * mult / 2)),
-                          nn.ReLU(True)]
+                          nn.ReLU(False)]
             else:
                 model += [Upsample(ngf * mult),
                           nn.Conv2d(ngf * mult, int(ngf * mult / 2),
@@ -984,7 +994,7 @@ class ResnetGenerator(nn.Module):
                                     padding=1,  # output_padding=1,
                                     bias=use_bias),
                           norm_layer(int(ngf * mult / 2)),
-                          nn.ReLU(True)]
+                          nn.ReLU(False)]
         model += [nn.ReflectionPad2d(3)]
         model += [nn.Conv2d(ngf, output_nc, kernel_size=7, padding=0)]
         model += [nn.Tanh()]
@@ -1054,7 +1064,7 @@ class ResnetDecoder(nn.Module):
                                              padding=1, output_padding=1,
                                              bias=use_bias),
                           norm_layer(int(ngf * mult / 2)),
-                          nn.ReLU(True)]
+                          nn.ReLU(False)]
             else:
                 model += [Upsample(ngf * mult),
                           nn.Conv2d(ngf * mult, int(ngf * mult / 2),
@@ -1062,7 +1072,7 @@ class ResnetDecoder(nn.Module):
                                     padding=1,
                                     bias=use_bias),
                           norm_layer(int(ngf * mult / 2)),
-                          nn.ReLU(True)]
+                          nn.ReLU(False)]
         model += [nn.ReflectionPad2d(3)]
         model += [nn.Conv2d(ngf, output_nc, kernel_size=7, padding=0)]
         model += [nn.Tanh()]
@@ -1100,7 +1110,7 @@ class ResnetEncoder(nn.Module):
         model = [nn.ReflectionPad2d(3),
                  nn.Conv2d(input_nc, ngf, kernel_size=7, padding=0, bias=use_bias),
                  norm_layer(ngf),
-                 nn.ReLU(True)]
+                 nn.ReLU(False)]
 
         n_downsampling = 2
         for i in range(n_downsampling):  # add downsampling layers
@@ -1108,11 +1118,11 @@ class ResnetEncoder(nn.Module):
             if(no_antialias):
                 model += [nn.Conv2d(ngf * mult, ngf * mult * 2, kernel_size=3, stride=2, padding=1, bias=use_bias),
                           norm_layer(ngf * mult * 2),
-                          nn.ReLU(True)]
+                          nn.ReLU(False)]
             else:
                 model += [nn.Conv2d(ngf * mult, ngf * mult * 2, kernel_size=3, stride=1, padding=1, bias=use_bias),
                           norm_layer(ngf * mult * 2),
-                          nn.ReLU(True),
+                          nn.ReLU(False),
                           Downsample(ngf * mult * 2)]
 
         mult = 2 ** n_downsampling
@@ -1164,7 +1174,7 @@ class ResnetBlock(nn.Module):
         else:
             raise NotImplementedError('padding [%s] is not implemented' % padding_type)
 
-        conv_block += [nn.Conv2d(dim, dim, kernel_size=3, padding=p, bias=use_bias), norm_layer(dim), nn.ReLU(True)]
+        conv_block += [nn.Conv2d(dim, dim, kernel_size=3, padding=p, bias=use_bias), norm_layer(dim), nn.ReLU(inplace=False)]
         if use_dropout:
             conv_block += [nn.Dropout(0.5)]
 
@@ -1249,9 +1259,9 @@ class UnetSkipConnectionBlock(nn.Module):
             input_nc = outer_nc
         downconv = nn.Conv2d(input_nc, inner_nc, kernel_size=4,
                              stride=2, padding=1, bias=use_bias)
-        downrelu = nn.LeakyReLU(0.2, True)
+        downrelu = nn.LeakyReLU(0.2, False)
         downnorm = norm_layer(inner_nc)
-        uprelu = nn.ReLU(True)
+        uprelu = nn.ReLU(False)
         upnorm = norm_layer(outer_nc)
 
         if outermost:
@@ -1310,9 +1320,9 @@ class NLayerDiscriminator(nn.Module):
         kw = 4
         padw = 1
         if(no_antialias):
-            sequence = [nn.Conv2d(input_nc, ndf, kernel_size=kw, stride=2, padding=padw), nn.LeakyReLU(0.2, True)]
+            sequence = [nn.Conv2d(input_nc, ndf, kernel_size=kw, stride=2, padding=padw), nn.LeakyReLU(0.2, False)]
         else:
-            sequence = [nn.Conv2d(input_nc, ndf, kernel_size=kw, stride=1, padding=padw), nn.LeakyReLU(0.2, True), Downsample(ndf)]
+            sequence = [nn.Conv2d(input_nc, ndf, kernel_size=kw, stride=1, padding=padw), nn.LeakyReLU(0.2, False), Downsample(ndf)]
         nf_mult = 1
         nf_mult_prev = 1
         for n in range(1, n_layers):  # gradually increase the number of filters
@@ -1322,13 +1332,13 @@ class NLayerDiscriminator(nn.Module):
                 sequence += [
                     nn.Conv2d(ndf * nf_mult_prev, ndf * nf_mult, kernel_size=kw, stride=2, padding=padw, bias=use_bias),
                     norm_layer(ndf * nf_mult),
-                    nn.LeakyReLU(0.2, True)
+                    nn.LeakyReLU(0.2, False)
                 ]
             else:
                 sequence += [
                     nn.Conv2d(ndf * nf_mult_prev, ndf * nf_mult, kernel_size=kw, stride=1, padding=padw, bias=use_bias),
                     norm_layer(ndf * nf_mult),
-                    nn.LeakyReLU(0.2, True),
+                    nn.LeakyReLU(0.2, False),
                     Downsample(ndf * nf_mult)]
 
         nf_mult_prev = nf_mult
@@ -1336,7 +1346,7 @@ class NLayerDiscriminator(nn.Module):
         sequence += [
             nn.Conv2d(ndf * nf_mult_prev, ndf * nf_mult, kernel_size=kw, stride=1, padding=padw, bias=use_bias),
             norm_layer(ndf * nf_mult),
-            nn.LeakyReLU(0.2, True)
+            nn.LeakyReLU(0.2, False)
         ]
 
         sequence += [nn.Conv2d(ndf * nf_mult, 1, kernel_size=kw, stride=1, padding=padw)]  # output 1 channel prediction map
@@ -1366,10 +1376,10 @@ class PixelDiscriminator(nn.Module):
 
         self.net = [
             nn.Conv2d(input_nc, ndf, kernel_size=1, stride=1, padding=0),
-            nn.LeakyReLU(0.2, True),
+            nn.LeakyReLU(0.2, False),
             nn.Conv2d(ndf, ndf * 2, kernel_size=1, stride=1, padding=0, bias=use_bias),
             norm_layer(ndf * 2),
-            nn.LeakyReLU(0.2, True),
+            nn.LeakyReLU(0.2, False),
             nn.Conv2d(ndf * 2, 1, kernel_size=1, stride=1, padding=0, bias=use_bias)]
 
         self.net = nn.Sequential(*self.net)

train_ddp.py
@@ -14,7 +14,7 @@ from data.unaligned_dataset import UnalignedDataset # Shit to be handled
 
 
 def setup(rank, world_size):
-    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_ADDR'] = 'localhost' # Only suitable for single node. replace with real addresses for multinode setup
     os.environ['MASTER_PORT'] = '12357'  # Use a free port
     dist.init_process_group("nccl", rank=rank, world_size=world_size)
     torch.cuda.set_device(rank)
@@ -30,6 +30,7 @@ def move_dict_to_device(batch, device):
     }
 
 def train(rank, world_size):
+    torch.autograd.set_detect_anomaly(True)
     is_distributed = world_size > 1
     if is_distributed:
         setup(rank, world_size)
@@ -46,9 +47,26 @@ def train(rank, world_size):
     my_model.data_dependent_initialize(first_batch)
     my_model.setup(opt) # regular setup: load and print networks; create schedulers
     
-    ddp_model = DDP(my_model, device_ids=[rank], find_unused_parameters=True) if is_distributed else my_model
-    model = ddp_model.module if is_distributed else ddp_model
-    
+    if is_distributed:
+        # Wrap the model with DistributedDataParallel
+        # Note: find_unused_parameters=True is used if your model has branches that may not be used in every forward pass
+        # This is useful for models with multiple outputs or conditional branches.
+        # If your model does not have such branches, you can set find_unused_parameters=False for better performance.
+        # If you are not sure, you can start with find_unused_parameters=True and then optimize later.
+        my_model.netG = DDP(my_model.netG, device_ids=[rank], find_unused_parameters=False)
+        if hasattr(my_model, 'netD'):
+            my_model.netD = DDP(my_model.netD, device_ids=[rank], find_unused_parameters=False)
+        if hasattr(my_model, 'netF'):
+            my_model.netF = DDP(my_model.netF, device_ids=[rank], find_unused_parameters=False)
+        if hasattr(my_model, 'netC'):
+            my_model.netC = DDP(my_model.netC, device_ids=[rank], find_unused_parameters=False)  
+
+
+    # ddp_model = DDP(my_model, device_ids=[rank], find_unused_parameters=True) if is_distributed else my_model
+    # model = ddp_model.module if is_distributed else ddp_model
+    model = my_model  # Use the model directly, as it is already wrapped in DDP if needed
+
+
     if rank == 0:  # Only print dataset size from the main process
         print('The number of training images = %d' % dataset_size)
         visualizer = Visualizer(opt)   # create a visualizer that display/save images and plots